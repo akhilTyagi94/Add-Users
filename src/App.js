474692bc75fbdcb786612209f2d24b32615c2fa0
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-07-11 01:00:10 +0530

    error modal initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
+import ErrorModal from "./components/UI/ErrorModal";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
